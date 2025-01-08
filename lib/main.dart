30b087eb417eb7fec3f5ef83e36b62945a7a01c0
3boodev <a.abdelsamad.al@gmail.com>
2025-01-08 02:32:01 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
+
       providers: [
         ChangeNotifierProvider(create: (context) => ThemeProvider()),
       ],
